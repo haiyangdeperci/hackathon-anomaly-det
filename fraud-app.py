c7da83844033e3eccfd2f98db1a3fb30b6bb9c85
haiyangdeperci <31899203+haiyangdeperci@users.noreply.github.com>
2022-02-18 10:42:20 +0100

    change endpoint

--- fraud-app.py
+++ fraud-app.py
@@ -10,15 +10,20 @@ import numpy as np
 
 PROJECT = "71149810252"
 LOCATION = "us-west1"
+ENDPOINT_NAME = "1785993911601201152"
 
 def init_endpoint(endpoint_name):
     aiplatform.init(project=PROJECT, location=LOCATION) # , 
-    endpoint = aiplatform.Endpoint(endpoint_name="5253765624676483072")
+    endpoint = aiplatform.Endpoint(endpoint_name=endpoint_name)
     return endpoint
 
 def predict(endpoint, payload):
-    prediction_obj = endpoint.predict(payload)
-    predictions = np.array(prediction_obj.predictions, dtype=np.float16)
+    # prediction_obj = endpoint.predict(payload)
+    explanation_obj = endpoint.explain(payload)
+    # print(explanation_obj)
+    # print(prediction_obj, print(endpoint), print(dir(endpoint)))
+    predictions = np.array(explanation_obj.predictions, dtype=np.float16)
+    explanations = explanation_obj.explanations
     isFraud = predictions >= 0.5
     result = pd.DataFrame({"prediction": predictions, "isFraud": isFraud})
     st.text("Results:")
@@ -30,7 +35,9 @@ st.write("Dear Consultant, please upload your batch")
 batch = st.file_uploader("CSV Batch upload", type=["csv"])
 if batch:
     payload = pd.read_csv(batch)
+    print(payload.shape)
     st.table(payload)
+    payload = payload.to_numpy().tolist()
     # mock payload
     # payload = [
     #     [1.00, 3.00, 1864.28, 21249.00, 19384.72, 0, 0, 0],
